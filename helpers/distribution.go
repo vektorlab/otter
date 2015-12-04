9ea77e0a39c961b441d83af5eb0a660dad651be1
Kevin Schoon <kevinschoon@gmail.com>
2015-12-04 14:10:29 -0500

    Moved distribution detection into seperate file, improved functionality

---
+++ helpers/distribution.go
@@ -0,0 +1,60 @@
+package helpers
+
+import (
+	"fmt"
+	"runtime"
+	"gopkg.in/ini.v1"
+)
+
+
+type Distro struct {
+	Family string // Major distribution type currently "debian" or "centos"
+	InitSystem string // The Linux init system used by this operating system
+	Version string // Major version of the distribution // TODO
+}
+
+func (d *Distro) SetFamily(family string) error {
+
+	switch family {
+	case "debian":
+		d.Family = family
+	case "centos":
+		d.Family = family
+	default:
+		return fmt.Errorf("Unknown Linux distribution: %s", family)
+	}
+	return nil
+}
+
+func (d *Distro) SetInitSystem() error {
+
+	switch d.Family {
+	case "debian":
+		d.InitSystem = "systemd" // TODO: Base off distribution.Version
+	case "centos":
+		d.InitSystem = "sysv"
+	default:
+		return fmt.Errorf("Unknown Init system for distribution: %s", d.Family)
+	}
+	return nil
+}
+
+func GetDistro() (*Distro, error) {
+	d := Distro{}
+	if runtime.GOOS == "linux" {
+		i, err := ini.Load([]byte(""), "/etc/os-release")
+		section, err := i.Section("").GetKey("ID_LIKE")
+		if err != nil {
+			return nil, err
+		}
+		err = d.SetFamily(section.String())
+		if err != nil {
+			return nil, err
+		}
+		err = d.SetInitSystem()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &d, nil
+}
