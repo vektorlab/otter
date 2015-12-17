01970de3892f3e348a006aeb6eb58de1ddf6f914
Kevin Schoon <kevinschoon@gmail.com>
2015-12-16 21:08:05 -0500

    Use etcd to register hosts and exchange state information

---
+++ daemon/daemon.go
@@ -0,0 +1,130 @@
+package daemon
+
+import (
+	"fmt"
+	"github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+type Daemon struct {
+	etcd     client.KeysAPI
+	index    uint64
+	firstRun bool
+}
+
+func (daemon *Daemon) register() error {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	options := client.SetOptions{
+		TTL: 60 * time.Second,
+	}
+	response, err := daemon.etcd.Set(context.Background(), fmt.Sprintf("/hosts/%s/host", hostname), hostname, &options)
+	if err != nil {
+		return err
+	}
+
+	if daemon.firstRun {
+		daemon.index = response.Index
+		daemon.firstRun = false
+	} else {
+		if daemon.index+1 != response.Index {
+			return fmt.Errorf("Another host is already registered with this hostname")
+		} else {
+			daemon.index = response.Index
+		}
+	}
+
+	return nil
+}
+
+func (daemon *Daemon) synchronize() {
+	err := daemon.register()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	time.Sleep(15 * time.Second)
+	daemon.Run()
+}
+
+func (daemon *Daemon) maintainState() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+	response, err := daemon.etcd.Get(context.Background(), fmt.Sprintf("/hosts/%s/state", hostname), nil)
+	if err != nil {
+		log.Println("Etcd error: ", err.Error())
+	} else {
+		fmt.Println(response.Node.Value)
+	}
+}
+
+func (daemon *Daemon) LoadState(state string) error {
+
+	hosts, err := daemon.ListHosts()
+
+	if err != nil {
+		return err
+	}
+
+	for _, host := range hosts {
+		response, err := daemon.etcd.Set(context.Background(), fmt.Sprintf("/hosts/%s/state", host), state, &client.SetOptions{})
+		if err != nil {
+			return err
+		}
+		log.Printf("Set host: %s %s", response.Action, host)
+	}
+
+	return nil
+}
+
+func (daemon *Daemon) ListHosts() ([]string, error) {
+
+	var hosts []string
+
+	response, err := daemon.etcd.Get(context.Background(), "/hosts", &client.GetOptions{Recursive: true})
+
+	if err != nil {
+		return hosts, err
+	}
+
+	for _, node := range response.Node.Nodes {
+		hosts = append(hosts, strings.Split(node.Key, "/")[2])
+	}
+
+	return hosts, nil
+}
+
+func (daemon *Daemon) Run() {
+	go daemon.synchronize()
+	go daemon.maintainState()
+	select {}
+}
+
+func NewDaemon(servers []string) (*Daemon, error) {
+	daemon := Daemon{
+		firstRun: true,
+	}
+
+	cfg := client.Config{
+		Endpoints: servers,
+		Transport: client.DefaultTransport,
+	}
+
+	c, err := client.New(cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	daemon.etcd = client.NewKeysAPI(c)
+
+	return &daemon, nil
+}
--- otter.go
+++ otter.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"github.com/olekukonko/tablewriter"
+	"github.com/vektorlab/otter/daemon"
 	"github.com/vektorlab/otter/state"
 	"os"
 	"strconv"
+	"strings"
 )
 
 var (
 	command  string
 	file     string
 	dumpJson bool
+	etcdUrl  string
 )
 
 func boolToColor(b bool) *color.Color {
@@ -22,6 +25,15 @@ func boolToColor(b bool) *color.Color {
 	} else {
 		return color.New(color.FgHiRed)
 	}
+}
+
+func getLoader() *state.Loader {
+	loader, err := state.FromPath(file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return loader
 }
 
 func dumpResults(results []state.Result) {
@@ -48,9 +60,26 @@ func dumpResults(results []state.Result) {
 	table.Render()
 }
 
+func dumpHosts(hosts []string) {
+	td := make([][]string, len(hosts))
+
+	for _, host := range hosts {
+		c := boolToColor(true).SprintfFunc()
+		td = append(td, []string{c(host)})
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"host"})
+
+	for _, v := range td {
+		table.Append(v)
+	}
+	table.Render()
+}
+
 func usage(options []string) {
 	fmt.Println("Otter is an opinionated configuration management framework for servers that run containers\n")
-	fmt.Println("Usage: otter [OPTIONS] [ls] \n")
+	fmt.Println("Usage: otter [OPTIONS] [load, ls, state, execute, daemon] \n")
 	fmt.Println("Options:")
 	for i := 0; i < len(options); i++ {
 		f := flag.Lookup(options[i])
@@ -59,49 +88,77 @@ func usage(options []string) {
 		}
 	}
 	fmt.Println("\nCommands:")
-	fmt.Println(" ls	Output the state from the Otter configuration file")
+	fmt.Println(" load	Load a state configuration into etcd")
+	fmt.Println(" ls	List all hosts registered in an etcd cluster")
 	fmt.Println(" state	Show the state of your operating system")
 	fmt.Println(" execute	Execute the state file against your operating system")
+	fmt.Println(" daemon	Run Otter in daemon mode")
 }
 
 func main() {
 
-	options := []string{"c"}
+	options := []string{"c", "e"}
 	flag.NewFlagSet("Otter", flag.ExitOnError)
-	flag.StringVar(&command, "Command", "", "Otter command [ls, state]")
+	flag.StringVar(&command, "Command", "", "Otter command [ls, state, execute, daemon]")
 	flag.StringVar(&file, "c", "otter.yml", "The path to an Otter state file")
+	flag.StringVar(&etcdUrl, "e", "http://127.0.0.1:2379", "URL to etcd hosts")
 	flag.BoolVar(&dumpJson, "json", false, "Dump state output to JSON")
 
 	flag.Parse()
 	flag.Usage = func() { usage(options) }
 
-	stateLoader, err := state.FromPath(file)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
 	switch flag.Arg(0) {
+	case "load":
+		loader := getLoader()
+		out, err := loader.Dump()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		daemon, err := daemon.NewDaemon(strings.Split(etcdUrl, ","))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		err = daemon.LoadState(string(out))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	case "ls":
-		out, err := stateLoader.Dump()
+		daemon, err := daemon.NewDaemon(strings.Split(etcdUrl, ","))
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println(string(out))
+		hosts, err := daemon.ListHosts()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		dumpHosts(hosts)
 	case "state":
-		err = stateLoader.Consistent()
+		loader := getLoader()
+		err := loader.Consistent()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		dumpResults(stateLoader.Results)
+		dumpResults(loader.Results)
 	case "execute":
-		err = stateLoader.Execute()
+		loader := getLoader()
+		err := loader.Execute()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
+	case "daemon":
+		daemon, err := daemon.NewDaemon(strings.Split(etcdUrl, ","))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		daemon.Run()
 	default:
 		flag.Usage()
 		os.Exit(1)
