106a573753b79cf9cd626ed5370a66078722a2e9
Kevin Schoon <kevinschoon@gmail.com>
2016-01-02 16:30:10 -0500

    Add ResultMap to results module

--- state/result.go
+++ state/result.go
@@ -10,19 +10,36 @@ type Result struct {
 	Message    string    // A message returned by the state
 }
 
-func ResultsToJson(results []Result) ([]byte, error) {
-	data, err := json.Marshal(results)
-	if err != nil {
-		return []byte(``), err
-	}
-	return data, nil
-}
-
 func ResultsFromJson(data []byte) ([]Result, error) {
 	results := make([]Result, 0)
 	err := json.Unmarshal(data, &results)
 	if err != nil {
-		return nil, err
+		return results, err
 	}
 	return results, nil
+}
+
+func ResultsToJson(results []Result) ([]byte, error) {
+	var data []byte
+	data, err := json.Marshal(results)
+	if err != nil {
+		return data, err
+	}
+	return data, nil
+}
+
+type ResultMap struct {
+	Results map[string][]Result
+}
+
+func (rm *ResultMap) Add(host string, result Result) {
+	rm.Results[host] = append(rm.Results[host], result)
+}
+
+func (rm *ResultMap) ToJSON() ([]byte, error) {
+	data, err := json.Marshal(rm.Results)
+	if err != nil {
+		return []byte(``), err
+	}
+	return data, nil
 }
