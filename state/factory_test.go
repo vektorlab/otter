e3ef7fb4caf751d0b10a6f26fd70e2993c2244d0
Kevin Schoon <kevinschoon@gmail.com>
2016-01-02 21:20:10 -0500

    Add basic test for StateFactory

---
+++ state/factory_test.go
@@ -0,0 +1,29 @@
+package state
+
+import (
+	"fmt"
+	"testing"
+)
+
+var simpleFile = []byte(`
+{"path": "/tmp/no-exist", "mode": 644}
+`)
+
+var simpleFileMeta = Metadata{
+	Name: "Simple File",
+	Type: "file",
+	State: "rendered",
+}
+
+func TestStateFactory(t *testing.T) {
+	state, err := StateFactory(simpleFileMeta, simpleFile)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	name := state.Meta().Name
+	if name != "Simple File" {
+		fmt.Printf("Did not load metadata correctly: %s", name)
+		t.Fail()
+	}
+}
