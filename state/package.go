a720e70e99b4776abe1307768ba5352615a3c010
Kevin Schoon <kevinschoon@gmail.com>
2016-01-16 17:39:53 -0500

    Update package state

--- state/package.go
+++ state/package.go
@@ -51,27 +51,43 @@ func (pkg *Package) Consistent() *Result {
 		Metadata:   &pkg.Metadata,
 		Consistent: false,
 	}
-	status, err := GetPackageStatus(pkg.Name)
+	status, version, err := GetPackageStatus(pkg.Name)
 	if err != nil {
 		result.Message = err.Error()
 		return result
 	}
-	result.Consistent = status == pkg.Metadata.State
+	switch result.Metadata.State {
+	case "installed":
+		if status == "installed" {
+			if pkg.Version != "" {
+				result.Consistent = version == pkg.Version
+			}
+		}
+	case "removed":
+		result.Consistent = status == ""
+	}
 	return result
 }
 
 func (pkg *Package) Execute() *Result {
-	result := &Result{
-		Metadata:   &pkg.Metadata,
-		Consistent: pkg.Consistent().Consistent,
-	}
-	if result.Consistent == false {
-		err := InstallPackage(pkg.Name)
+	result := pkg.Consistent()
+	switch {
+	case result.Consistent == true:
+		return result
+	case pkg.Metadata.State == "installed":
+		err := InstallPackage(pkg.Name, pkg.Version)
 		if err != nil {
 			result.Message = err.Error()
 			return result
 		}
-		result.Consistent = pkg.Consistent().Consistent
+		result.Message = "Package Installed"
+	case pkg.Metadata.State == "removed":
+		err := RemovePackage(pkg.Name)
+		if err != nil {
+			result.Message = err.Error()
+			return result
+		}
+		result.Message = "Package Removed"
 	}
 	return result
 }
@@ -79,10 +95,10 @@ func (pkg *Package) Execute() *Result {
 /*
 Get the status of a package in DPKG
 */
-func GetDpkgPackage(name string) (string, error) {
+func GetDpkgPackage(name string) (string, string, error) {
 	out, err := exec.Command("dpkg", "-l", name).Output() // TODO: Security
 	if err != nil {                                       // dpkg doesn't know about the package
-		return "rc", nil
+		return "rc", "", nil
 	}
 	lines := strings.Split(string(out), "\n")
 	for _, line := range lines {
@@ -90,40 +106,40 @@ func GetDpkgPackage(name string) (string, error) {
 		if len(split) >= 1 {
 			f := string(split[0][0])
 			if f == "h" || f == "i" || f == "p" || f == "r" || f == "u" {
-				return split[0], nil
+				return split[0], split[2], nil
 			}
 		}
 	}
-	return "", nil
+	return "", "", nil
 }
 
 /*
 Check to see if a package is installed on the operating system
 */
-func GetPackageStatus(name string) (string, error) {
+func GetPackageStatus(name string) (string, string, error) {
 	distro, err := helpers.GetDistro()
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 
 	switch distro.Family {
 	case "debian":
-		status, err := GetDpkgPackage(name)
+		status, version, err := GetDpkgPackage(name)
 		if err != nil {
-			return "", err
+			return "", "", err
 		}
 		switch status {
 		case "ii":
-			return "installed", nil
+			return "installed", version, nil
 		case "rc":
-			return "removed", nil
+			return "removed", version, nil
 		case "":
-			return "purged", nil
+			return "purged", version, nil
 		default:
-			return "", fmt.Errorf("Package %s is in an unknown state: %s", name, status)
+			return "", "", fmt.Errorf("Package %s is in an unknown state: %s", name, status)
 		}
 	default:
-		return "", fmt.Errorf("Unsupported operating system: %s", distro.Family)
+		return "", "", fmt.Errorf("Unsupported operating system: %s", distro.Family)
 	}
 }
 
@@ -136,26 +152,61 @@ func installAptPackage(name string) error {
 		log.Warningln(string(out))
 		return err
 	}
-	log.Warningln(string(out))
 	out, err = exec.Command("apt-get", "install", "-y", name).CombinedOutput()
+	if err != nil {
+		log.Warningln(string(out))
+		return err
+	}
+	log.Println("Installed Apt package: ", string(out))
+	return nil
+}
+
+/*
+Remove a package with apt-get
+ */
+func removeAptPackage(name string) error {
+	out, err := exec.Command("apt-get", "remove", name).CombinedOutput()
 	if err != nil {
 		return err
 	}
-	log.Warningln(string(out))
+	log.Println("Removed Apt package: ", out)
 	return nil
 }
 
 /*
 Install a package on the operating system
 */
-func InstallPackage(name string) error {
+func InstallPackage(name, version string) error {
 	distro, err := helpers.GetDistro()
 	if err != nil {
 		return err
 	}
 	switch distro.Family {
 	case "debian":
+		if version != "" {
+			name = name + fmt.Sprintf("==%s", version)
+		}
 		err := installAptPackage(name)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("Unsupported operating system: %s", distro.Family)
+	}
+	return nil
+}
+
+/*
+Remove a package from the operating system
+ */
+func RemovePackage(name string) error {
+	distro, err := helpers.GetDistro()
+	if err != nil {
+		return err
+	}
+	switch distro.Family {
+	case "debian":
+		err := removeAptPackage(name)
 		if err != nil {
 			return err
 		}
