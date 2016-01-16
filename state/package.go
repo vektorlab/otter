c6ae18a7ce8a8f746fd738c568484260abbc7b69
Kevin Schoon <kevinschoon@gmail.com>
2016-01-16 16:50:27 -0500

    Move package helper functions to state

--- state/package.go
+++ state/package.go
@@ -10,7 +10,10 @@ package state
 import (
 	"encoding/json"
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/vektorlab/otter/helpers"
+	"os/exec"
+	"strings"
 )
 
 type Package struct {
@@ -45,10 +48,10 @@ func (pkg *Package) Meta() Metadata {
 
 func (pkg *Package) Consistent() *Result {
 	result := &Result{
-		Metadata: &pkg.Metadata,
+		Metadata:   &pkg.Metadata,
 		Consistent: false,
 	}
-	status, err := helpers.GetPackageStatus(pkg.Name)
+	status, err := GetPackageStatus(pkg.Name)
 	if err != nil {
 		result.Message = err.Error()
 		return result
@@ -59,11 +62,11 @@ func (pkg *Package) Consistent() *Result {
 
 func (pkg *Package) Execute() *Result {
 	result := &Result{
-		Metadata: &pkg.Metadata,
+		Metadata:   &pkg.Metadata,
 		Consistent: pkg.Consistent().Consistent,
 	}
 	if result.Consistent == false {
-		err := helpers.InstallPackage(pkg.Name)
+		err := InstallPackage(pkg.Name)
 		if err != nil {
 			result.Message = err.Error()
 			return result
@@ -71,4 +74,93 @@ func (pkg *Package) Execute() *Result {
 		result.Consistent = pkg.Consistent().Consistent
 	}
 	return result
+}
+
+/*
+Get the status of a package in DPKG
+*/
+func GetDpkgPackage(name string) (string, error) {
+	out, err := exec.Command("dpkg", "-l", name).Output() // TODO: Security
+	if err != nil {                                       // dpkg doesn't know about the package
+		return "rc", nil
+	}
+	lines := strings.Split(string(out), "\n")
+	for _, line := range lines {
+		split := strings.Fields(line)
+		if len(split) >= 1 {
+			f := string(split[0][0])
+			if f == "h" || f == "i" || f == "p" || f == "r" || f == "u" {
+				return split[0], nil
+			}
+		}
+	}
+	return "", nil
+}
+
+/*
+Check to see if a package is installed on the operating system
+*/
+func GetPackageStatus(name string) (string, error) {
+	distro, err := helpers.GetDistro()
+	if err != nil {
+		return "", err
+	}
+
+	switch distro.Family {
+	case "debian":
+		status, err := GetDpkgPackage(name)
+		if err != nil {
+			return "", err
+		}
+		switch status {
+		case "ii":
+			return "installed", nil
+		case "rc":
+			return "removed", nil
+		case "":
+			return "purged", nil
+		default:
+			return "", fmt.Errorf("Package %s is in an unknown state: %s", name, status)
+		}
+	default:
+		return "", fmt.Errorf("Unsupported operating system: %s", distro.Family)
+	}
+}
+
+/*
+Install a package with apt-get
+*/
+func installAptPackage(name string) error {
+	out, err := exec.Command("apt-get", "update").CombinedOutput()
+	if err != nil {
+		log.Warningln(string(out))
+		return err
+	}
+	log.Warningln(string(out))
+	out, err = exec.Command("apt-get", "install", "-y", name).CombinedOutput()
+	if err != nil {
+		return err
+	}
+	log.Warningln(string(out))
+	return nil
+}
+
+/*
+Install a package on the operating system
+*/
+func InstallPackage(name string) error {
+	distro, err := helpers.GetDistro()
+	if err != nil {
+		return err
+	}
+	switch distro.Family {
+	case "debian":
+		err := installAptPackage(name)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("Unsupported operating system: %s", distro.Family)
+	}
+	return nil
 }
